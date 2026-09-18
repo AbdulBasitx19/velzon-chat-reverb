69e31ca0c1c620a296a2ecb85c89f31316102381
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-18 16:59:31 +0500

    feat : create ChatController with index and store method to display users and store message when clicked on send message

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\User;
+use App\Events\MessageSent;
+
+class ChatController extends Controller
+{
+    //
+    public function index()
+    {
+        $users = User::where('id', '!=', auth()->id())->get();
+
+        return view('chat.index', compact('users'));
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'message' => 'required|string|max:1000',
+            'receiver_id' => 'required|exists:users,id',
+        ]);
+
+        $message = Message::create([
+            'sender_id' => auth()->id(),
+            'receiver_id' => $request['receiver_id'],
+            'message' => $request['message'],
+        ]);
+
+        event(new MessageSent($message));
+
+        // ajax k liay response
+        return response()->json([
+            'success' => 'true',
+            'message' => $message->load('sender'), // Sender ki details bhej di
+        ]);
+    }
+
+}
